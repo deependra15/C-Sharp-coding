b3945d142c69931edf4433576ebf222eb1c39c4d
deependra15 <56729604+deependra15@users.noreply.github.com>
2019-12-22 20:17:23 +0530

    Added Diamond Pattern file
    This is a Program of Diamond Pattern on C#

---
+++ atm1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace diamondpattern
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			{
+				int i, j, count = 1, number;
+				Console.Write("Enter number of rows:");
+				number = int.Parse(Console.ReadLine());
+				count = number - 1;
+				for (j = 1; j <= number; j++)
+				{
+					for (i = 1; i <= count; i++)
+						Console.Write("*");
+
+					for (i = 1; i <= 2 * j - 1; i++)
+						Console.Write("#");
+					for (i = 1; i <= count; i++)
+						Console.Write("*");
+					Console.WriteLine();
+					count--;
+				}
+				count = 1;
+				for (j = 1; j <= number - 1; j++)
+				{
+					for (i = 1; i <= count; i++)
+						Console.Write("*");
+					count++;
+						Console.WriteLine();
+					}
+					Console.ReadLine();
+				}
+			}
+		}
+	}
+
+
